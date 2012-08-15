4c7c7f8283c6ed301b9d3b93e0eeadcae9dba5c8
Futuro <evan@cs.umn.edu>
2012-08-15 17:56:08 -0500

    Added more exploration code
    I have added some more code to help familiarize myself with the various
    perl modules I need to achieve SASLy goodness.

--- sasl-test.pl
+++ sasl-test.pl
@@ -7,21 +7,47 @@ use Authen::Krb5;
 use Authen::SASL;
 use Net::LDAP;
 use IO::Socket;
+use Term::ReadLine;
+use Term::ReadKey;
+use GSSAPI;
+
+my $host = $ARGV[0] || die "\nusage: $0 <ldapserver> <user>\n\n";
+my $user = $ARGV[1] || die "\nusage: $0 <ldapserver> <user>\n\n";
 
 my $SERVICE = "krbtgt";
-my $SERVER = "neo.cose.umn.edu";
 
-Authen::Krb5::init_context();
+my $krb5con = Authen::Krb5::init_context();
+my $princ = Authen::Krb5::parse_name($user);
 
+print "Getting creds for '".$princ->data."@".$princ->realm."'\n";
+print "Please enter your password: ";
+ReadMode('noecho');
+my $pass = ReadLine(0);
+chomp $pass;
+ReadMode('restore');
+print "\n";
+
+my $creds = Authen::Krb5::get_init_creds_password($princ, $pass);
+my $ccache = Authen::Krb5::cc_resolve("FILE:/tmp/plaidjoin_sasltest");
+#my $ccache = Authen::Krb5::cc_default();
+Authen::Krb5::Ccache::initialize($ccache, $princ);
+$ccache->store_cred($creds);
+
+## XXX: This is currently the only way I know of to pass a non-default ticket location
+##      to SASL. In Authen::SASL::Perl::GSSAPI(3m), it speaks of passing in a GSSAPI::Cred object
+##      via the Authen::SASL callback hash using the 'pass' key. Diggin into the GSSAPI perl module,
+##      and the GSSAPI documentation as well, I haven't found a good way to use a different location
+##      for the keytab aside from setting KRB5CCNAME. There is no way to initialize a new GSSAPI::Cred
+##      object to pass in anywhere. I am simply at a loss as to how to do this any other way
+$ENV{KRB5CCNAME}="/tmp/plaidjoin_sasltest";
+print "\$KRB5CCNAME is \"$ENV{KRB5CCNAME}\"\n";
 my $sasl = Authen::SASL->new(
             mechanism => 'GSSAPI',
-            callback => {
-                user => \&getuser,
-                pass => \&getpass,
-                auth => \&getauth,
-            });
+            #callback => {
+                #auth => $ccache
+            #},
+        );
 
-my $host = $ARGV[0] || die "\nusage: $0 <ldapserver> \n\n";
 my $ldap = Net::LDAP->new(
         $host,
         onerror => 'die' ,
