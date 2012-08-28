9b53c4c1976472a1981c12db3632e8036310ef78
Futuro <evan@cs.umn.edu>
2012-08-28 15:55:39 -0500

    Cleaned up 'lib/Encryption.pm'
    I removed all code that dealt with DES and ARCFOUR encryption types, as
    those encryption types are not valid with the server that I'm working
    with. In my continued effort to trim out any code that is either not
    necessary or is untestable, this code got the axe.
    
    In the 'deduce_linux_enc_info' and 'deduce_solaris_enc_info'
    subroutines, I also pulled the code that tests the OS value into tiny
    if-blocks at the top of the sub-routine. These if-blocks will return
    from the routines if activated.
    
    I also clarified various comments surrounding the affected code.

--- lib/Encryption.pm
+++ lib/Encryption.pm
@@ -29,8 +29,6 @@ BEGIN {
 #       is that then I have two exit points from the routine, which can increase work if
 #       I every decide that the exit value should change. Maybe the readability tradeoff is
 #       worth it though...
-# NOTE: This will look for aes, des and arcfour support, though it will only report
-#       the min and max key size for aes.
 # Input:
 #   N/A
 # Output:
@@ -38,53 +36,47 @@ BEGIN {
 sub deduce_linux_enc_info {
     my %enc_info;
 
-    if ($^O eq "linux") {
-        my @modules = qx(lsmod);
-        MODULE:
-        foreach (@modules) {
-            next unless /^(?:aes|des|arc4)/;
-
-            # Check if we have aes support, and if so, find out our min and max
-            # key size, converting from bytesize to bitsize.
-            if (/^aes/) {
-                ${enc_info}{aes} = 1;
-
-                # It's possible that 'aes' will show up more than once, such as 'aes_x86_64'
-                # and 'aes_generic'. If we've already found the necessary info, we don't need
-                # to look for it again.
-                next if (${enc_info}{minkeysize} and ${enc_info}{maxkeysize});
-
-                my $in_aes;
-
-                my @crypto = qx'cat /proc/crypto|grep "name\|keysize"';
-                foreach (@crypto) {
-                    next unless (/^name\s+:\s+aes/ or $in_aes);
-
-                    # If we hit a new name section, we're no longer 'in_aes', and should skip until
-                    # we hit a new aes section (if any)
-                    if (/^name\s+:\s+(?!aes)/) {
-                        $in_aes = 0;
-                        next;
-                    }
-
-                    $in_aes = 1;
-
-                    /^min keysize\s+:\s+(\d+)/ and ${enc_info}{minkeysize} = ($1 * 8);
-
-                    /^max keysize\s+:\s+(\d+)/ and ${enc_info}{maxkeysize} = ($1 * 8);
-
-                    # If we've found the key sizes, don't hang about.
-                        last if (${enc_info}{minkeysize} and ${enc_info}{maxkeysize});
-                }
-            }
-
-            (${enc_info}{des} = 1)  if (/^des/);
-            (${enc_info}{arc4} = 1) if (/^arc4/);
-        }
-    }
-    else {
+    if ($^O ne "linux") {
         print qq(This function should only be called from a linux system, but \$^O says $^O\n);
         print qq(No encryption types found\n);
+        return %enc_info;
+    }
+
+    my @modules = qx(lsmod);
+    MODULE:
+    foreach (@modules) {
+        # Check if we have aes support, and if so, find out our min and max
+        # key size, converting from bytesize to bitsize.
+        if (/^aes/) {
+            ${enc_info}{aes} = 1;
+
+            # It's possible that 'aes' will show up more than once, such as 'aes_x86_64'
+            # and 'aes_generic'. If we've already found the necessary info, we don't need
+            # to look for it again.
+            next if (${enc_info}{minkeysize} and ${enc_info}{maxkeysize});
+
+            my $in_aes;
+
+            my @crypto = qx'cat /proc/crypto|grep "name\|keysize"';
+            foreach (@crypto) {
+                next unless (/^name\s+:\s+aes/ or $in_aes);
+
+                # If we hit a new name section, we're no longer 'in_aes', and should skip until
+                # we hit a new aes section (if any)
+                if (/^name\s+:\s+(?!aes)/) {
+                    $in_aes = 0;
+                    next;
+                }
+
+                $in_aes = 1;
+
+                /^min keysize\s+:\s+(\d+)/ and ${enc_info}{minkeysize} = ($1 * 8);
+                /^max keysize\s+:\s+(\d+)/ and ${enc_info}{maxkeysize} = ($1 * 8);
+
+                # If we've found the key sizes, don't hang about.
+                    last if (${enc_info}{minkeysize} and ${enc_info}{maxkeysize});
+            }
+        }
     }
 
     return %enc_info;
@@ -100,21 +92,19 @@ sub deduce_linux_enc_info {
 sub deduce_solaris_enc_info {
     my %enc_info = {};
 
-    if ($^O eq "solaris") {
-        my @crypto = qx(encrypt -l);
-        foreach (@crypto) {
-            if (/^aes\s+(\d+)\s+(\d+)/) {
-                ${enc_info}{aes} = 1;
-                ${enc_info}{minkeysize} = $1;
-                ${enc_info}{maxkeysize} = $2;
-            }
-            (${enc_info}{des} = 1)  if (/^des/);
-            (${enc_info}{arc4} = 1) if (/^arcfour/);
-        }
-    }
-    else {
+    if ($^O ne "solaris") {
         print qq(This function should only be called from a solaris system, but \$^O says $^O\n);
         print qq(No encryption types found\n);
+        return %enc_info;
+    }
+
+    my @crypto = qx(encrypt -l);
+    foreach (@crypto) {
+        if (/^aes\s+(\d+)\s+(\d+)/) {
+            ${enc_info}{aes} = 1;
+            ${enc_info}{minkeysize} = $1;
+            ${enc_info}{maxkeysize} = $2;
+        }
     }
 
     return %enc_info;
@@ -148,7 +138,7 @@ sub deduce_and_set_enc_types {
         print "This function currently only supports linux and solaris OSs.\n";
         print "However, you're running $^O, according to perl.\n";
         print "I haven't really decided what to do in this case, so let's just die.\n";
-        die   "Maybe add support for $^O in subroutine 'set_enc_type'.\n";
+        die   "Maybe add support for $^O in subroutine 'deduce_and_set_enc_type'.\n";
     }
 
     if (${enc_info}{minkeysize} == 128 and ${enc_info}{maxkeysize} == 256) {
@@ -175,12 +165,10 @@ sub deduce_and_set_enc_types {
         $result = ldapreplace( $ldap, $distinct_name, { 'msDS-SupportedEncryptionTypes' => $val } );
         if ($result->code){
             warn "Failed to replace entry msDS-SupportedEncryptionTypes: ", $result->error;
+            warn "LDAP error code: ".$result->code."\n";
+            warn "LDAP error text: ".$result->error_text."\n\n";
             warn "AES doesn't seem to be supported by the domain controller.\n";
-            # XXX: When I tested this, neither of those encryption types worked
-            warn "Assuming 1DES and Arcfour encryption types.\n";
-
-            $aes128_supported = 0;
-            $aes256_supported = 0;
+            die  "No other encryption types are currently supported; dying.\n";
         }
     }
 
@@ -190,24 +178,9 @@ sub deduce_and_set_enc_types {
 
     $aes256_supported and print "AES-256 is supported.\n";
 
-    # Apparently AD prefers Arcfour, so that comes next
-    # XXX: High chance this statement is wrong, TODO in another branch
-    if (${enc_info}{arc4}) {
-        push @enc_types, "arcfour-hmac-md5";
-        print "Arcfour is supported.\n";
-    }
-    else {
-        print "Arcfour is not supported.\n";
-    }
-
-    if (${enc_info}{des}) {
-        push @enc_types, "des-cbc-crc";
-        push @enc_types, "des-cbc-md5";
-    }
-
     if (!@enc_types) {
-        warn "No encryption types supported.\n";
-        die  "Please enable AES-128, AES-256, Arcfour, or DES support, then re-join.\n";
+        warn "No encryption types supported on the client.\n";
+        die  "Please enable AES-256/AES-128 then re-join.\n";
     }
 
     return @enc_types;
